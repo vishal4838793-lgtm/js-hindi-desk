95c44a3f0a42dbe2c60d2178e219e6e8fe2478d2
vishal4838793-lgtm <vishal4838793@gmail.com>
2026-07-02 18:08:48 +0000

    date and time completed

---
+++ 01_basic/02_dateandtime.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2026, 5, 12)
+console.log(myCreatedDate.toDateString()
