69aaa53a051a5d7dfc7f02b2e39fa1d7cc9ca3fd
vishal4838793-lgtm <vishal4838793@gmail.com>
2026-07-10 18:23:55 +0000

    variables,datatypes,stack and heap

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 16578398
+let accountEmail = "vishal@4838793gmail.com"
+var accountPassword = "12345"
+accountCity = "Sonipat"
+let accountState = ""
+// accountId = 2
+
+accountPassword = "137392"
+accountCity = "jaipur"
+// console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
+
+
---
+++ 01_basic/03_datatype.js
@@ -0,0 +1,18 @@
+let score = "52abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// *********************Operation**************//
+
+let value = 3
+let anotherNo = -value
+// console.log(anotherNo);
+
+str1 = "Hey"
+str2 = " Vishi"
+str3 = str1+str2
+console.log(str3);
